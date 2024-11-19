e963e5c515c37ee3be67931c602618890c052c2c
NavalNorth <moha_naval@live.com>
2024-11-19 20:49:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -34,21 +34,6 @@ app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 
 
-const db = mysql.createConnection({
-    host: isProduction ? process.env.PROD_DB_HOST : '127.0.0.1',
-    user: isProduction ? process.env.PROD_DB_USER : process.env.DB_USER,
-    password: isProduction ? process.env.PROD_DB_PASSWORD : process.env.DB_PASSWORD,
-    database: isProduction ? process.env.PROD_DB_NAME : process.env.DB_NAME,
-    port: isProduction ? (process.env.PROD_PORT || 3306) : 3306, // Défaut sur 3306
-});
-
-console.log('Tentative de connexion avec les paramètres suivants :', {
-    host: isProduction ? process.env.PROD_DB_HOST : '127.0.0.1',
-    user: isProduction ? process.env.PROD_DB_USER : process.env.DB_USER,
-    database: isProduction ? process.env.PROD_DB_NAME : process.env.DB_NAME,
-    port: isProduction ? process.env.PROD_PORT : 3306,
-});
-
 db.connect((err) => {
     if (err) {
         console.log('ERREUR');
