7fe2e85d5e2c8bf08a3ac071eba7bedef018f8f1
NavalNorth <moha_naval@live.com>
2024-11-19 21:51:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(cors({origin: "http://localhost:8080"}));
 app.use(bodyParser.json())
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME, 
-    port: process.env.DB_PORT || 3306, 
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER, 
+    password: process.env.MYSQLPASSWORD, 
+    database: process.env.MYSQLDATABASE, 
+    port: process.env.MYSQLPORT || 3306, 
 });
 
 
